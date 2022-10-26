5dca93f25d2e587db86bff4a92150b307a649a78
Areum Park <areumi3690@gmail.com>
2022-10-26 23:19:19 +0900

    2022-10-26

--- src/main/webapp/5_jquery_class/1_menuPan과제/SB2.js
+++ src/main/webapp/5_jquery_class/1_menuPan과제/SB2.js
@@ -1,46 +1,73 @@
 $(function() {
-
-	$(".tab_content").hide(); //Hide all content
-	$("ul.tabs li:first").addClass("active").show(); //Activate first tab
-	$(".tab_content:first").show(); //Show first tab content
+	// 탭화면
+	$(".tab_content").hide(); // 원하는 화면 외 다른 컨텐츠 숨기기
+	$("ul.tabs li:first").addClass("active").show(); // 첫번째 탭 활성화
+	$(".tab_content:first").show(); // 첫번째 탭안의 내용들을 보여준다.
 
 
 	$("ul.tabs li").click(function() {
 
-		$("ul.tabs li").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected tab
-		$(".tab_content").hide(); //Hide all tab content
+		$("ul.tabs li").removeClass("active"); // 이전 탭 활성화 클레스 제거
+		$(this).addClass("active"); // 선택한 탭의 활성화 부여
+		$(".tab_content").hide(); // 선택한 탭 외의 다른 컨텐츠 숨기기
 
-		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-		$(activeTab).fadeIn(); //Fade in the active ID content
+		var activeTab = $(this).find("a").attr("href"); // 선택한 a테그 의 하이퍼링크 착기
+		$(activeTab).fadeIn(); // 탭에 포커스 맞추기 ()
 		return false;
 	});
-
+	
+	// 윈도우 화면고정
 	$(window).scroll(function() {
 		$('#rightdiv').css('top', $(document).scrollTop())
 	});
 
-	let sum = 0;
+	// 주문내역 저장및 출력
+	var sum = 0;
 	$('.menuCount').change(function() {
-
+	
+		
 		let menu = $(this).parent().find('span:eq(0)').attr('value');
 		let price = $(this).parent().find('span:eq(1)').attr('value');
 		let count = $(this).val();
+		
+		
+		if(count==0){
+			alert('1개 이상 선택해 주세요');
+		}else{
 		sum += count * price;
 		$('#total').attr('value', sum);
 
-		let tr = $('<tr/ class="jumun">');
-
+		let tr = $('<tr/ class="jumun">');	// tr 저장변수
+		
+		// 중복일때 
+		let flag = false;
+		let list = $('#listTable tr.jumun');
+		for(let i=0; i<list.length; i++){
+			if(menu==$(list[i]).find('td:eq(0)').text()){
+				let Count = Number($(list[i]).find('td:eq(1)').text());
+				Count += Number(count);
+				$(list[i]).find('td:eq(1)').text(Count);
+				
+				let Price = Number($(list[i]).find('td:eq(2)').text());
+				Price += Number(price) * Number(count);
+				$(list[i]).find('td:eq(2)').text(Price);
+				flag = true;
+			}
+		}
+		
+		if(!flag){
 		tr.append($('<td/>').text(menu));
 		tr.append($('<td/>').text(count));
 		tr.append($('<td/>').text(price));
 		tr.append($('<td><button class="bttn">삭제</button></td>'));
 
 		$('#listTable').append(tr);
-
+		}
+		}
 	});
 
-	$('#listTable').on('click', '.bttn', function() {
+	$('#listTable ').on('click', '.bttn', function() {
+
 		let price = $(this).parent().parent().find('td:eq(2)').text();
 		let count = $(this).parent().parent().find('td:eq(1)').text();
 		sum -= price * count;
@@ -49,19 +76,32 @@ $(function() {
 
 	});
 
+
 	$('#btn').click(function() {
-		let result = confirm("주문 하시겠습니까?");
+		let price = $(this).parent().parent().find('td:eq(2)').text();
+		let count = $(this).parent().parent().find('td:eq(1)').text();
+		sum += count * price;
+		let result = confirm(sum + "원입니다. 주문 하시겠습니까?");
 		if (!result) {
 			$('.jumun').remove();
 			$('#total').attr('value', 0);
-			$(".menuCount > option:eq(0)").prop("selected", true);
+			sum = 0;
 			return false;
 		} else {
 			$('.jumun').remove();
 			$('#total').attr('value', 0);
-
-		}
+			$(".menuCount").val('0');
+			sum = 0;
+			winObj = window.open("jumun.html", "", 'width=310, height=150');
+		}	
 	});
+	
+	
+	
+	$('#card').click(function(){
+			window.close();
+			
+		});
 
 
 })
